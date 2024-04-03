5906cd1b281a6426576eedb20d4cf3e43d4da959
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2024-04-03 19:37:33 +0900

    更新 test.ts

--- test.ts
+++ test.ts
@@ -1,11 +1,18 @@
-basic.forever(function() {
-    angle8.setRGB(0, 255, 0, 0, angle8.input8(0));
-    angle8.setRGB(1, 255, 255, 0, angle8.input8(1));
-    angle8.setRGB(2, 255, 0, 255, angle8.input8(2));
-    angle8.setRGB(3, 0, 255, 0, angle8.input8(3));
-    angle8.setRGB(4, 0, 255, 255, angle8.input8(4));
-    angle8.setRGB(5, 0, 0, 255, angle8.input8(5));
-    angle8.setRGB(6, 255, 255, 255, angle8.input8(6));
-    angle8.setRGB(7, 255, 0, 0, angle8.input8(7));
-    angle8.setRGB(8, 0, 255, 0, angle8.inputSW()*32);
+let colors = [
+    angle8.getColor(0, 0, 255),
+    angle8.getColor(0, 255, 0),
+    angle8.getColor(0, 255, 255),
+    angle8.getColor(255, 0, 0),
+    angle8.getColor(255, 0, 255),
+    angle8.getColor(255, 255, 0),
+    angle8.getColor(255, 255, 255),
+    angle8.getColor(0, 0, 255),
+    angle8.getColor(0, 255, 0)
+]
+basic.forever(function () {
+    for (let カウンター = 0; カウンター <= 7; カウンター++) {
+        angle8.setColor(カウンター, colors[カウンター], (255 - angle8.input8(カウンター)) / 8)
+    }
+    angle8.setColor(8, colors[8], angle8.inputSW() * 32)
+    basic.pause(100)
 })
